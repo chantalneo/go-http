4d517840eef6c070e0a4147d5973911a5b6117fa
Chantal Neo <Chantal@hotmail.sg>
2020-04-01 11:21:15 +0800

    Quiz 10: Test Your Knowledge: Interfaces

--- main.go
+++ main.go
@@ -246,3 +246,82 @@ func (logWriter) Write(bs []byte) (int, error) {
 // 	      }
 // 	      return written, err
 //     }
+
+// Quiz 10: Test Your Knowledge: Interfaces
+// 1. When we say that interfaces can be satisfied implicitly, we mean that... We don't have to write any extra code to say that some type satisfies an interface
+// 2. To say that a type satisfies an interface means that... The type implements all of the functions contain in the interface definition
+// 3. In the following code, does the square type satisfy the shape interface? Yes, because square defines the area function that returns an int
+/* 	type shape interface {
+    	area() int
+	}
+
+	type square struct {
+    	sideLength int
+	}
+
+	func (s square) area() int {
+    	return s.sideLength * s.sideLength
+	}
+
+	func printArea(s shape) {
+    	fmt.Println(s.area())
+	} */
+// 4. Take a look at the following code. Does the rectangle type satisfy the shape interface? No, because rectangle's version of the area function returns a float64,
+//    but the shape interface expects a return type of int
+/* 	type shape interface {
+    	area() int
+	}
+
+	type square struct {
+    	sideLength int
+	}
+
+	type rectangle struct {
+    	height float64
+    	width float64
+	}
+
+	func (s square) area() int {
+    	return s.sideLength * s.sideLength
+	}
+
+	func (r rectangle) area() float64 {
+    	return r.height * r.width
+	}
+
+	func printArea(s shape) {
+    	fmt.Println(s.area())
+	} */
+// 5. Take a look at the following code. Type square appears to successfully implement the shape interface, but the implementation of square's area function looks broken
+//    - it always returns a value of 10 no matter what the side length of the square is.
+//    Will the shape interface do anything to help us catch this error? No, interfaces are only used to help with types. We can still easily write code that does something
+//    completely wrong
+/* 	type shape interface {
+    	area() int
+	}
+
+	type square struct {
+    	sideLength int
+	}
+
+	func (s square) area() int {
+    	return 10
+	}
+
+	func printArea(s shape) {
+    	fmt.Println(s.area())
+	} */
+// 6. Types that implement the Reader interface are generally used to... Read information from an outside source into our application
+// 7. Imagine that you ask a coworker to create a new type that implements the Reader interface to take data from a text file and print it on the screen.
+//    They present you with the following code:
+/* 	type textFileReader struct {}
+
+	func (textFileReader) Read(bs []byte) (int, error) {
+    	return "Information from a text file"
+	} */
+//    They say that this code successfully compiled, so it must be correct. You then review their code and give them feedback. What would you say?
+//    I would say that while the textFileReader type conforms to the requirement of the Reader interface, it doesn't actually implement the desired
+//    behavior of reading a file from the hard drive
+// 8. Because interfaces are satisfied implicitly, it can be tough to figure out which types satisfy which interfaces.
+//    Take a look at the type File inside the os package here: https://golang.org/pkg/os/#File
+//    Does the File type satisfy both the Reader and Writer interfaces? Yes
