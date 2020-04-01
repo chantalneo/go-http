86e448e71937c64769e0a986c0d667814a27060c
Chantal Neo <Chantal@hotmail.sg>
2020-04-01 10:56:33 +0800

    The Implementation of io.Copy

--- main.go
+++ main.go
@@ -177,3 +177,62 @@ func main() {
 //                    Therefore, it implements the Writer interface
 //
 // That's why we can pass os.Stdout to io.Copy
+//
+// 7. Actual Source Code of io.Copy
+//    func Copy(dst Writer, src Reader) (written int64, err error) {
+// 	     return copyBuffer(dst, src, nil)
+//    }
+//
+//    func CopyBuffer(dst Writer, src Reader, buf []byte) (written int64, err error) {
+// 	     if buf != nil && len(buf) == 0 {
+// 		    panic("empty buffer in io.CopyBuffer")
+// 	     }
+// 	     return copyBuffer(dst, src, buf)
+//    }
+//
+// 	  func copyBuffer(dst Writer, src Reader, buf []byte) (written int64, err error) {
+//       // If the reader has a WriteTo method, use it to do the copy.
+// 	     // Avoids an allocation and a copy.
+// 	     if wt, ok := src.(WriterTo); ok {
+// 		    return wt.WriteTo(dst)
+// 	     }
+// 	     // Similarly, if the writer has a ReadFrom method, use it to do the copy.
+// 	     if rt, ok := dst.(ReaderFrom); ok {
+// 		    return rt.ReadFrom(src)
+// 	     }
+// 	     if buf == nil {
+// 		    size := 32 * 1024
+// 		    if l, ok := src.(*LimitedReader); ok && int64(size) > l.N {
+// 			   if l.N < 1 {
+// 				  size = 1
+// 			   } else {
+// 			      size = int(l.N)
+// 			   }
+// 		    }
+// 		    buf = make([]byte, size)
+// 	     }
+// 	     for {
+// 		    nr, er := src.Read(buf)
+// 		    if nr > 0 {
+// 			   nw, ew := dst.Write(buf[0:nr])
+// 			      if nw > 0 {
+// 				     written += int64(nw)
+// 			      }
+// 			      if ew != nil {
+// 				     err = ew
+// 				     break
+// 			      }
+// 			      if nr != nw {
+// 				     err = ErrShortWrite
+// 				     break
+// 			      }
+// 		    }
+// 		    if er != nil {
+// 			   if er != EOF {
+// 			      err = er
+// 			   }
+// 			   break
+// 		    }
+// 	      }
+// 	      return written, err
+//     }
